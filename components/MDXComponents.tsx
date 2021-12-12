1413fd6d71cc5ba85fceab1594cc98a5d580dd17
Lourival Cavalcante <cavalcantebsi@gmail.com>
2021-12-12 00:17:32 -0400

    first basic changes

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -3,11 +3,6 @@ import Image from 'next/image';
 
 import ProsCard from 'components/ProsCard';
 import ConsCard from 'components/ConsCard';
-import Gumroad from 'components/metrics/Gumroad';
-import Unsplash from 'components/metrics/Unsplash';
-import Analytics from 'components/metrics/Analytics';
-import YouTube from 'components/metrics/Youtube';
-import Step from 'components/Step';
 import ImageWithTheme from 'components/ImageWithTheme';
 
 const CustomLink = (props) => {
@@ -33,13 +28,8 @@ const MDXComponents = {
   Image: RoundedImage,
   ImageWithTheme,
   a: CustomLink,
-  Analytics,
   ConsCard,
-  Gumroad,
   ProsCard,
-  Step,
-  Unsplash,
-  YouTube
 };
 
 export default MDXComponents;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 
 import Container from '../components/Container';
 import BlogPostCard from '../components/BlogPostCard';
-import Subscribe from '../components/Subscribe';
 import VideoCard from '../components/VideoCard';
 
 export default function Home({ videos }) {
@@ -13,66 +12,47 @@ export default function Home({ videos }) {
         <div className="flex flex-col-reverse sm:flex-row items-start">
           <div className="flex flex-col pr-8">
             <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-              Lee Robinson
+              Lourival Cavalcante
             </h1>
             <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-              Head of Developer Relations at{' '}
-              <span className="font-semibold">Vercel</span>
+              Senior QA Consultant @{''}
+              <span className="font-semibold">Doji</span>
             </h2>
             <p className="text-gray-600 dark:text-gray-400 mb-16">
-              Helping developers build a faster web. Teaching about web
-              development, serverless, and React / Next.js.
+              Helping to develop quality software in agile contexts.
             </p>
           </div>
           <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
             <Image
-              alt="Lee Robinson"
-              height={176}
-              width={176}
-              src="/avatar.jpg"
+              alt="Lourival Cavalcante"
+              height={256}
+              width={256}
+              src="/avatar.jpeg"
               className="rounded-full filter grayscale"
             />
           </div>
         </div>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
-          Featured Posts
+          Principal Projects
         </h3>
         <div className="flex gap-6 flex-col md:flex-row">
           <BlogPostCard
-            title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
+            title="in progress"
             slug="style-guides-component-libraries-design-systems"
             gradient="from-[#D8B4FE] to-[#818CF8]"
           />
           <BlogPostCard
-            title="Rust Is The Future of JavaScript Infrastructure"
+            title="in progress"
             slug="rust"
             gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
           />
           <BlogPostCard
-            title="Past, Present, and Future of React State Management"
+            title="in progress"
             slug="react-state-management"
             gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
           />
         </div>
-        <Link href="/blog">
-          <a className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6">
-            Read all posts
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="h-6 w-6 ml-1"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
-              />
-            </svg>
-          </a>
-        </Link>
+        
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
           Learn React & Next.js
         </h3>
@@ -128,7 +108,6 @@ export default function Home({ videos }) {
           </svg>
         </a>
         <span className="h-16" />
-        <Subscribe />
       </div>
     </Container>
   );
